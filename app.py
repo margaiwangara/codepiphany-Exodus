d95ebed96338baf466e5c12905bf3c5901c04e21
= <=>
2018-03-15 15:22:06 +0300

    second-design initial commit

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
     return render_template('Index.html')
+    if request.method == "POST":
+        #get data from the form and send to db
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        if message is not None and name is not None and email is not None:
+            app.logger.info("Message sent successfully")
+        else:
+            app.logger.info("Message not sent")
 
 if __name__ == '__main__':
     app.run(debug=True)
